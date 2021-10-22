cfb21cbe912ff9d41087e17b4416ca01334ac2fe
Matthew Ho <msho@ucsb.edu>
2021-10-22 11:02:15 -0700

    added bad question generation

--- causal_sentence.py
+++ causal_sentence.py
@@ -1,9 +1,21 @@
 import wikipediaapi
 import json
+from enum import Enum
 from tqdm import tqdm
 
+
 wiki = wikipediaapi.Wikipedia('en')
-causal_words = ['because', 'thus', 'as a result of', 'due to']
+
+class CausalOrder(Enum):
+    PRECEDING = 1     # x [word] y, x causes y
+    SUCCEEDING = 2    # x [word] y, y causes x
+causal_words = {
+    'because': CausalOrder.PRECEDING,
+    'due to': CausalOrder.PRECEDING,
+    'thus': CausalOrder.SUCCEEDING,
+    'therefore': CausalOrder.SUCCEEDING,
+    'causes': CausalOrder.SUCCEEDING,
+}
 
 catname = 'Category:Earth_phenomena'
 cat = wiki.page(catname)
@@ -14,10 +26,10 @@ def get_category_members(category):
             if c.ns != wikipediaapi.Namespace.CATEGORY]
 
 def has_causal_word(sentence):
-    for word in causal_words:
+    for word in causal_words.keys():
         if (' ' + word + ' ') in sentence:
-            return True
-    return False
+            return True, word
+    return False, ''
 
 def add_causal_sentences(name, page_to_sentence, add_context=False, count=[0]):
     page = wiki.page(name)
@@ -26,16 +38,20 @@ def add_causal_sentences(name, page_to_sentence, add_context=False, count=[0]):
     if len(sentences) == 0:
         return
 
+    # last "sentence" is blank context for first sentence
+    sentences.append('');
     page_sentences = []
-    # if add_context is true, include the previous sentence as context
-    if add_context:
-        page_sentences = [sentences[i - 1] + '. ' + sentences[i]
-            for i in range(1, len(sentences))
-                if has_causal_word(sentences[i])]
-        if has_causal_word(sentences[0]):
-            page_sentences.append(sentences[0])
-    else:
-        page_sentences = [sentence for sentence in sentences if has_causal_word(sentence)]
+    for i in range(len(sentences) - 1):
+        has_word, word = has_causal_word(sentences[i])
+        if has_word and add_context:
+            page_sentences.append({
+                'context': sentences[i - 1],
+                'sentence': sentences[i],
+                'keyword': word})
+        elif has_word:
+            page_sentences.append({
+                'sentence': sentences[i],
+                'keyword': word})
 
     # print('causal_sentences:', len(page_sentences))
     if len(page_sentences):
@@ -51,13 +67,44 @@ def causal_sentences_from_category(catname, add_context=False):
     for i in tqdm(range(len(pages))):
         add_causal_sentences(pages[i], res, add_context, count)
     print('Gathered', count[0], 'sentences')
+    res['Sentence Count'] = count[0]
     return res
 
-csentences = causal_sentences_from_category(catname, add_context=False)
-json_f = json.dumps(csentences, indent=2)
-fname = 'res.json'
-f = open(fname, 'w')
-f.write(json_f)
-f.close()
+def generate_question(entry):
+    words = entry['sentence'].split(' ')
+    keyword_index = words.index(entry['keyword'].split(' ')[0])
+    causal_order = causal_words[entry['keyword']];
+    if causal_order == CausalOrder.SUCCEEDING:
+        before = ' '.join(words[:keyword_index])
+        return 'why does ' + before + '?'
+    else:
+        after = ' '.join(words[keyword_index + len(entry['keyword'].split(' ')):])
+        return 'why does ' + after + '?'
 
-print('Successfully written to', fname)
+fname = 'res.json'
+csentences = {}
+
+WRITE_TO_JSON = False
+if WRITE_TO_JSON:
+    csentences = causal_sentences_from_category(catname, add_context=True)
+    json_f = json.dumps(csentences, indent=2)
+    f = open(fname, 'w')
+    f.write(json_f)
+    f.close()
+    print('Successfully written to', fname)
+else:
+    f = open(fname)
+    csentences = json.load(f)
+    f.close()
+    print('Successfully read from', fname)
+
+# print(generate_question(csentences['Air shower (physics)'][0]))
+
+counter = 0
+limit = 20
+for pagename in csentences:
+    for sentence in csentences[pagename]:
+        print(generate_question(sentence))
+        counter += 1
+    if counter > limit:
+        break        
